3e98670e0c49edece4b56b777e88b965098eddc8
vadik <vadik.olympus@e1.ru>
2018-12-21 14:09:44 +0500

    edit adapter

--- src/adapter/AdapterApp.java
+++ src/adapter/AdapterApp.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package adapter;
-
 /**
  *
  * @author KonovalovVA
  */
 public class AdapterApp {
-
     public static void main(String[] args) {
         DrawGraphicsInterface dr = new VectorAdapterFromRaster();
         dr.drawLine();
@@ -19,61 +12,37 @@ public class AdapterApp {
         dr.drawLine();
         dr.drawSquare();
     }
-
 }
-
-class VectorAdapterFromRaster2 implements DrawGraphicsInterface {
-
-    private VectorAdapterFromRaster2() {
-    }
-    private RasterGraphics rasterGraphics;
-
-    public VectorAdapterFromRaster2(RasterGraphics rasterGrahpics) {
-        this.rasterGraphics = rasterGrahpics;
-    }
-
-    @Override
-    public void drawLine() {
-        rasterGraphics.drawRasterLine();
-    }
-
-    @Override
-    public void drawSquare() {
-        rasterGraphics.drawRasterSquare();
-
-    }
-
-}
-
-class VectorAdapterFromRaster extends RasterGraphics implements DrawGraphicsInterface {
-
-    @Override
-    public void drawLine() {
-        drawRasterLine();
-    }
-
-    @Override
-    public void drawSquare() {
-        drawRasterSquare();
-    }
-
-}
-
 interface DrawGraphicsInterface {
-
     void drawLine();
-
     void drawSquare();
-
 }
-
 class RasterGraphics {
-
     void drawRasterLine() {
         System.out.println("Рисую линию");
     }
-
     void drawRasterSquare() {
         System.out.println("Рисую квадрат");
+    }
+}
+class VectorAdapterFromRaster2 implements DrawGraphicsInterface {
+    private VectorAdapterFromRaster2(){}
+    private RasterGraphics rasterGraphics;
+    public VectorAdapterFromRaster2(RasterGraphics rasterGrahpics){
+        this.rasterGraphics = rasterGrahpics;
+    }
+    public void drawLine() {
+        rasterGraphics.drawRasterLine();
+    }
+    public void drawSquare() {
+        rasterGraphics.drawRasterSquare();
+    }
+}
+class VectorAdapterFromRaster extends RasterGraphics implements DrawGraphicsInterface {
+    public void drawLine() {
+        drawRasterLine();
+    }
+    public void drawSquare() {
+        drawRasterSquare();
     }
 }
