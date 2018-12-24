f457a64859f9a58180b23701d44e4d921468a7a4
vadik <vadik.olympus@e1.ru>
2018-12-24 08:30:21 +0600

    add state

--- src/state/StateApp.java
+++ src/state/StateApp.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package state;
-
 /**
  *
  * @author Вадик
  */
 public class StateApp {
-    
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.setCurrentState(new ConcreteStateB());
+        context.setCurrentState(new ConcreteStateA());
+    }
+}
+class Context{
+    private State currentState;
+    public void setCurrentState(State currentState) {
+        this.currentState = currentState;
+        this.currentState.someMethod();
+    }
+}
+interface State{
+    void someMethod();
+}
+class ConcreteStateA implements State{
+    public void someMethod() {
+        System.out.println("now context in state A");
+    }
+}
+class ConcreteStateB implements State{
+    public void someMethod() {
+        System.out.println("now context in state B");
+    }
 }
