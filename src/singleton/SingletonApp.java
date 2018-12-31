a7c106ed20b1c89480b9ad5113c817282ca5e665
vadik <vadik.olympus@e1.ru>
2018-12-31 09:51:28 +0500

    edit singleton

--- src/singleton/SingletonApp.java
+++ src/singleton/SingletonApp.java
@@ -1,40 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package singleton;
-
 /**
  *
  * @author KonovalovVA
  */
 public class SingletonApp {
-
     public static void main(String[] args) {
         Singleton s = Singleton.getInstance();
-        System.out.println(SingletonApp.class);
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        
+        
     }
-
 }
-
 class Singleton {
-
-    private static Singleton instance;
-
-    private Singleton() {
-        System.out.println(this);
-    }
-
+    private Singleton() {}
+    private static final Object sync = new Object();
+    private static volatile Singleton instance = null;
     public static Singleton getInstance() {
         if (instance == null) {
-            synchronized (instance) {
+            synchronized (sync) {
                 if (instance == null) {
                     instance = new Singleton();
                 }
             }
         }
+        System.out.println("instance: " + instance.hashCode());
         return instance;
     }
-
 }
