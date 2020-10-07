0b04ec86269e9868f8e902173342937513bf2944
konovalov <v.a.konovalov@urfu.ru>
2020-10-07 10:41:46 +0500

    picked up creational gof patterns

--- src/creational/factoryMethod/FactoryMethodApp.java
+++ src/creational/factoryMethod/FactoryMethodApp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package factoryMethod;
+package creational.factoryMethod;
 
 import java.util.Date;
 
