0af7497cce2f4a6155a72fec9e50615254d5dbe0
konovalov <v.a.konovalov@urfu.ru>
2020-12-06 17:40:15 +0500

    add strategy

--- src/gof/behavior/strategy/StrategyApp.java
+++ src/gof/behavior/strategy/StrategyApp.java
@@ -1,51 +1,57 @@
-package strategy;
+package gof.behavior.strategy;
 
 import java.util.Arrays;
 
 /**
- *
  * @author Вадик
  */
 public class StrategyApp {
     public static void main(String[] args) {
         StrategyClient client = new StrategyClient();
-        int arr[]={3,1,5,9,7};
-        
+        int arr[] = {3, 1, 5, 9, 7};
+
         Strategy strategy = new SelectionSortStrategy(arr);
         client.setStrategy(strategy);
         client.performOperation();
-        
-        int arr1[]={3,1,5,9,7};
+
+        int arr1[] = {3, 1, 5, 9, 7};
         strategy = new BubleSortStrategy(arr1);
         client.setStrategy(strategy);
         client.performOperation();
-        
-        int arr2[]={3,1,5,9,7};
+
+        int arr2[] = {3, 1, 5, 9, 7};
         strategy = new InsertSortStrategy(arr2);
         client.setStrategy(strategy);
         client.performOperation();
     }
 }
-class StrategyClient{
+
+class StrategyClient {
     private Strategy strategy;
+
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
     }
-    public void performOperation(){
+
+    public void performOperation() {
         strategy.operation();
     }
 }
+
 interface Strategy {
     void operation();
 }
+
 class SelectionSortStrategy implements Strategy {
     int[] arr;
+
     public SelectionSortStrategy(int[] arr) {
         this.arr = arr;
     }
+
     public void operation() {
         System.out.println("Сортировка выборками");
-        System.out.println("до: "+Arrays.toString(arr));
+        System.out.println("до: " + Arrays.toString(arr));
         for (int barier = 0; barier < arr.length - 1; barier++) {
             for (int i = barier + 1; i < arr.length; i++) {
                 if (arr[i] < arr[barier]) {
@@ -55,17 +61,20 @@ class SelectionSortStrategy implements Strategy {
                 }
             }
         }
-         System.out.println("после: "+Arrays.toString(arr));
+        System.out.println("после: " + Arrays.toString(arr));
     }
 }
+
 class BubleSortStrategy implements Strategy {
     int[] arr;
+
     public BubleSortStrategy(int[] arr) {
         this.arr = arr;
     }
+
     public void operation() {
         System.out.println("Сортировка методом пузырька");
-        System.out.println("до: "+Arrays.toString(arr));
+        System.out.println("до: " + Arrays.toString(arr));
         for (int barier = arr.length - 1; barier >= 0; barier--) {
             for (int i = 0; i < barier; i++) {
                 if (arr[i] > arr[i + 1]) {
@@ -75,17 +84,20 @@ class BubleSortStrategy implements Strategy {
                 }
             }
         }
-        System.out.println("после: "+Arrays.toString(arr));
+        System.out.println("после: " + Arrays.toString(arr));
     }
 }
+
 class InsertSortStrategy implements Strategy {
     int[] arr;
+
     public InsertSortStrategy(int[] arr) {
         this.arr = arr;
     }
+
     public void operation() {
         System.out.println("Сортировка вставками");
-        System.out.println("до: "+Arrays.toString(arr));
+        System.out.println("до: " + Arrays.toString(arr));
         for (int barier = 1; barier < arr.length; barier++) {
             int index = barier;
             while (index - 1 >= 0 && arr[index] < arr[index - 1]) {
@@ -95,6 +107,6 @@ class InsertSortStrategy implements Strategy {
                 index--;
             }
         }
-        System.out.println("после: "+Arrays.toString(arr));
+        System.out.println("после: " + Arrays.toString(arr));
     }
 }
